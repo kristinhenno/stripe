5b22325834488cfa7e99dae55a5af45b0432dc96
Kristin Henno <kristinhenno@Kristins-MacBook-Pro.local>
2020-11-05 02:25:34 -0800

    styling done

---
+++ client/build/precache-manifest.443bdd9f6fc91f6ba8605bd8fd9375d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc67c80df0fbf2d53f19c8310bce1d81",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4990834d9b84520f8355",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "743f81898c1b3dd431e7",
+    "url": "/static/js/2.5297d7c4.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.5297d7c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4990834d9b84520f8355",
+    "url": "/static/js/main.3a643cda.chunk.js"
+  },
+  {
+    "revision": "c52961160cf8c09fa9be",
+    "url": "/static/js/runtime-main.3b9b6ece.js"
+  }
+]);
