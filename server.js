0e2f825bcb73fcb9e7a590bf8bb00279186bab0f
Kristin Henno <kristinhenno@Kristins-MacBook-Pro.local>
2020-11-05 16:13:34 -0800

    routes fixed with HashRouter

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.post('/create-checkout-session', async (req, res) => {
     line_items: data.line_items,
     mode: data.mode,
     allow_promotion_codes: true,
-    success_url: 'http://stripe-kristinhenno.herokuapp.com/success',
+    success_url: 'http://kristin-staging.herokuapp.com/#/success',
     cancel_url: 'http://stripe-kristinhenno.herokuapp.com',
   });
 
